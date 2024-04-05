73290fd356e0b8dca0faf4254712865f746eff27
JuanGustah <juangt360graus@gmail.com>
2024-04-05 13:01:24 -0300

    feat: add design system guidelines

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,35 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      lime: "#00FB00",
+      black: "#000000",
+      dark: "#000606",
+      white: "#FFFFFF",
+      snow: "#F6F6F6",
+      metal: "#D6D6D6",
+    },
+    fontWeight: {
+      regular: "400",
+      medium: "500",
+      "semi-bold": "600",
+      bold: "700",
+      "extra-bold": "800",
+    },
+    fontSize: {
+      xxs: "1rem",
+      xs: "1.25rem",
+      sm: "1.5rem",
+      md: "1.75rem",
+      lg: "2rem",
+      xl: "2.25rem",
+      "2xl": "3.25rem",
+      "4xl": "4.5rem",
+    },
+    fontFamily: {
+      montserrat: "Montserrat, sans-serif",
+      righteous: "Righteous, display",
+    },
     extend: {},
   },
   plugins: [],
