680f038829a3dd1c93b8ca41f0c0fb28fe24d16b
JuanGustah <juangt360graus@gmail.com>
2024-04-09 12:33:00 -0300

    chore: set useQuery defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+// import useFetch from "./hooks/useFetch";
+
 function App() {
+  // const { isSuccess, data } = useFetch({
+  //   key: "date",
+  //   value: "2024-04-09",
+  // });
+  // console.log(data);
+  // return isSuccess ? <h1>App</h1> : <p>aguarde</p>;
+
   return <h1>App</h1>;
 }
 
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -7,16 +7,17 @@ type queryParam = {
 };
 
 async function fetchHttp(queryParam: queryParam) {
-  const response = await api(`fixtures?${queryParam.key}=${queryParam.value}`);
+  const response = await api.get(
+    `fixtures?${queryParam.key}=${queryParam.value}`
+  );
   return response;
 }
 
-export default function useFetch(queryParam: queryParam, queryKeys: string[]) {
+export default function useFetch(queryParam: queryParam) {
   const query = useQuery({
-    queryFn: () => {
-      fetchHttp(queryParam);
-    },
-    queryKey: queryKeys,
+    queryFn: () => fetchHttp(queryParam),
+    queryKey: ["fixtures-by-date", queryParam.value],
+    staleTime: 60 * 60 * 24 * 1000,
   });
 
   return query;
