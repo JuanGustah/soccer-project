b2fab47c16e2951f66e84c25249b7179bdbaf7df
JuanGustah <juangt360graus@gmail.com>
2024-04-29 16:27:00 -0300

    feat: add routes to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Details from "./pages/Details.tsx";
+import Home from "./pages/Home.tsx";
 
 const client = new QueryClient({
   defaultOptions: {
@@ -12,8 +15,25 @@ const client = new QueryClient({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/details",
+        element: <Details />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={client}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
