b66a1e629e81931edb2449a1b4527b912439fca6
JuanGustah <juangt360graus@gmail.com>
2024-04-29 16:25:58 -0300

    refactor: isolate the fixtures list in another page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { AxiosResponse } from "axios";
+
+import Container from "../components/Container";
+import useFetch from "../hooks/useFetch";
+
+import { fixture } from "../types/fixture";
+
+import Background from "../assets/images/background_list.png";
+import Player from "../assets/icons/soccer_player.png";
+import FixtureList from "../components/FixtureList";
+
+type selectedFixtures = {
+  [key: number]: fixture[];
+};
+
+export default function Home() {
+  const selectedFixturesToday: selectedFixtures = {};
+  const selectedLeaguesId = [
+    4, 21, 71, 72, 75, 76, 39, 78, 140, 1, 2, 45, 48, 15, 480, 253, 37, 9, 556,
+    531, 73, 5, 624, 612, 475, 622, 632, 34, 13, 11, 3, 529, 143,
+  ];
+
+  function populateFixturesToday(data: AxiosResponse) {
+    const fixtures: fixture[] = data.data.response;
+
+    fixtures.forEach((fixture) => {
+      if (selectedLeaguesId.includes(fixture.league.id)) {
+        if (!selectedFixturesToday[fixture.league.id]) {
+          selectedFixturesToday[fixture.league.id] = [];
+        }
+        selectedFixturesToday[fixture.league.id].push(fixture);
+      }
+    });
+
+    console.log(selectedLeaguesId);
+    console.log(selectedFixturesToday);
+    console.log(data);
+  }
+
+  const { isSuccess, data } = useFetch({
+    key: "date",
+    value: new Date().toISOString().split("T")[0],
+  });
+
+  if (isSuccess) {
+    populateFixturesToday(data);
+  }
+
+  return (
+    <React.Fragment>
+      <img
+        src={Background}
+        alt="background image"
+        className="absolute inset-0 w-full z-negative"
+      />
+      <Container className="mt-14">
+        <span className="flex items-center gap-2">
+          <h2 className="font-extra-bold text-xl text-white">
+            Partidas de Hoje
+          </h2>
+          <img src={Player} alt="soccer player" width={30} height={30} />
+        </span>
+        {isSuccess ? (
+          <>
+            {Object.keys(selectedFixturesToday).map((fixtureId) => {
+              return (
+                <FixtureList
+                  fixtures={selectedFixturesToday[Number(fixtureId)]}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <></>
+        )}
+      </Container>
+    </React.Fragment>
+  );
+}
