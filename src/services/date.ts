50931c705dba1f42a264e81a62e5789f9408aed3
JuanGustah <juangt360graus@gmail.com>
2024-05-09 15:43:38 -0300

    chore: add date service

---
+++ src/services/date.ts
@@ -0,0 +1,25 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import calendar from "dayjs/plugin/calendar";
+import updateLocale from "dayjs/plugin/updateLocale";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import "dayjs/locale/pt-br";
+
+dayjs.extend(timezone);
+dayjs.extend(calendar);
+dayjs.extend(updateLocale);
+dayjs.extend(isSameOrAfter);
+dayjs.locale("pt-br");
+
+dayjs.updateLocale("pt-br", {
+  calendar: {
+    sameDay: "[Hoje]",
+    nextDay: "[Amanhã]",
+    nextWeek: "dddd",
+    sameElse: "DD/MM",
+  },
+});
+
+dayjs.tz.setDefault("America/Recife");
+
+export default dayjs;
