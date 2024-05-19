e8d1d7f093ccae3ec589bc1b68a13138afb238cb
JuanGustah <juangt360graus@gmail.com>
2024-05-18 21:44:22 -0300

    refactor: add parameters in fixtureResponse type

--- src/types/fixtureResponse.ts
+++ src/types/fixtureResponse.ts
@@ -2,6 +2,10 @@ import { fixture } from "./fixture";
 
 export type fixtureResponse = {
   get: string;
+  parameters: {
+    date?: string;
+    team?: number;
+  };
   errors: string[];
   results: number;
   paging: {
