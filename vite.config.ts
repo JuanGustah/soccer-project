f6ee8d7744007df9f918b250154a59141d2e940d
JuanGustah <juangt360graus@gmail.com>
2024-05-22 19:18:28 -0300

    chore: configure vitest and react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+/// <reference types="vitest">
+/// <reference types="vite/client">
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
